3d22b3aac02407ed8ae902771b97a38e35d61fd4
nickariley <nickariley@gmail.com>
2020-06-10 19:48:45 -0500

    updated students api

--- CS321_W2D2_StudentAPI/Services/StudentsService.cs
+++ CS321_W2D2_StudentAPI/Services/StudentsService.cs
@@ -58,27 +58,38 @@ namespace CS321_W2D2_StudentAPI.Services
         public Student Get(int id)
         {
             // return the specified Student or null if not found
+            return _students.FirstOrDefault(s => s.Id == id);
         }
 
         public IEnumerable<Student> GetAll()
         {
             // return all students
+            return _students;
         }
 
         public Student Update(Student updatedStudent)
         {
             // get the Student object in the current list with this id 
+            var currentStudent = this.Get(updatedStudent.Id);
 
             // return null if item to update isn't found
+            if (currentStudent == null) return null;
 
             // copy the property values from the updated student into the current student object
+            currentStudent.FirstName = updatedStudent.FirstName;
+            currentStudent.LastName = updatedStudent.LastName;
+            currentStudent.BirthDate = updatedStudent.BirthDate;
+            currentStudent.Email = updatedStudent.Email;
+            currentStudent.Phone = updatedStudent.Phone;
 
             // return student
+            return currentStudent;
         }
 
         public void Remove(Student student)
         {
             // remove student
+            _students.Remove(student);
         }
     }
 }
